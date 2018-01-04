d1295babf60f006e095e520c674ae959f0c850f8
iwanariy <yuki.iwnr@gmail.com>
2018-01-04 20:23:09 +0900

    fix install.sh for .gitconfig

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 dotfiles_base="$(cd $(dirname "$0") && pwd)"
 
+# Git
+git config --global user.name "$(cd "$dotfiles_base" && git log -1 --format=format:%an)"
+git config --global user.email "$(cd "$dotfiles_base" && git log -1 --format=format:%ae)"
+git config --global include.path "$dotfiles_base/.gitconfig"
+
 # Install dotfiles
 ln -snfv "$dotfiles_base/.zshrc" "$HOME/.zshrc"
 ln -snfv "$dotfiles_base/.vimrc" "$HOME/.vimrc"
